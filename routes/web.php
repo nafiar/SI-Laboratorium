147d53c4e3a2d7b1fada777685080253184eb0da
Unknown <unknown@example.com>
2017-05-02 23:57:17 +0700

    test tambah folder

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/home', 'FrontendUserController@index');
 
 Route::get('/', function () {
     return view('welcome');
