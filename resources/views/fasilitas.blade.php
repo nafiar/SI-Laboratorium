18e296bea84ba553a714a9c991753005061b1ef1
nadiarahma <nadiarahmatin@gmail.com>
2017-05-11 18:04:29 +0700

    edit halaman fasilitas, tambah gambar fasilitas

--- resources/views/fasilitas.blade.php
+++ resources/views/fasilitas.blade.php
@@ -5,42 +5,93 @@ Form Reservasi
 @endsection
 
 @section('moreStyle')
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 @endsection
 
 @section('content')
-	<div class="col-md-6 col-md-offset-3 waktu-home">
+<div class="container">
+	<h1>Fasilitas</h1>
+	<div class="col-md-12 form-reservasi">
 		<div class="panel panel-default panel-form">
 			<div class="panel-body">
-				<form>
-					<div class="form-group row">
-						<div class="col-md-6 waktu-jadwal">
-							<label class="control-label" for="focusedInput">Pilih Tanggal</label>
-							<input class="form-control waktu-jadwal" id="focusedInput datepicker" type="text" placeholder="">	
-						</div>
-						<div class="col-md-6 waktu-jadwal">
-							<label class="control-label" for="focusedInput">Pilih Jam</label>
-							<input class="form-control waktu-jadwal" id="focusedInput timepicker" type="text" placeholder="">
+				<div class="row">
+					<div class="col-md-4">
+						<div class="thumbnail">
+							<a href="">
+								<img class="img-responsive" src="/img/pc.png" style="width:100%" >
+							</a>
+							<div class="caption">
+								<h2 class="text-center">Personal Computer</h2><br>
+								<h5>Jumlah : 10 buah</h5>
+								<h5>Keterangan : Processor intel i3, VGA CARD 2Gb 64Bit, Hardisk Seagate 500Gb, LCD	Viewsonic 20 Inch, Keyboard LOGITECH, Wireless Mouse</h5>
+								<button type="button" class="btn btn-info btn-lg btn-group btn-group-justified" data-toggle="modal" data-target="#modal1">Pinjam</button>
+							</div>
+							<!-- Modal -->
+							<div class="modal fade" id="modal1" role="dialog">
+								<div class="modal-dialog">
+									<!-- Modal content-->
+									<div class="modal-content">
+										<div class="modal-header">
+											<button type="button" class="close" data-dismiss="modal">&times;</button>
+											<h4 class="modal-title">Personal Computer</h4>
+										</div>
+										<div class="modal-body">
+											<form class="form-horizontal" role="form">
+												<div class="form-group">
+													<label  class="col-sm-2 control-label" >Nama</label>
+													<div class="col-sm-10">
+														<input type="email" class="form-control"/>
+													</div>
+												</div>
+												<div class="form-group">
+													<label class="col-sm-2 control-label" >NRP</label>
+													<div class="col-sm-10">
+														<input type="password" class="form-control"/>
+													</div>
+												</div>
+												<div class="form-group">
+													<label class="col-sm-2 control-label" >No HP</label>
+													<div class="col-sm-10">
+														<input type="password" class="form-control"/>
+													</div>
+												</div>
+												<div class="form-group">
+													<label class="col-sm-2 control-label" >Email</label>
+													<div class="col-sm-10">
+														<input type="password" class="form-control"/>
+													</div>
+												</div>
+												<div class="form-group">
+													<label class="col-sm-2 control-label" >Lama Peminjaman</label>
+													<div class="col-sm-5">
+														<input type="datepicker" class="form-control" placeholder="Mulai"/>
+													</div>
+													<div class="col-sm-5">
+														<input type="datepicker" class="form-control" placeholder="Selesai"/>
+													</div>
+												</div>
+												<div class="form-group">
+													<label class="col-sm-2 control-label" >Keperluan</label>
+													<div class="col-sm-10">
+														<input type="password" class="form-control" placeholder="Alasan meminjam"/>
+													</div>
+												</div>
+												</form>
+											<button type="button" class="btn btn-default" data-dismiss="modal">Pinjam</button>
+										</div>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
-				</form>
-			</div>
-		</div>		
-	</div>
-	<div class="col-md-6 col-md-offset-3 schedule-hari">
-		<div class="panel panel-default schedule">
-			<h4><strong>Sabtu, 6 May 2017</strong></h4>
-			<br>
-			<div class="panel-body">
-				<div class="btn btn-block btn-link detail-schedule">
-					<h6>Praktikum Jarkom</h6>
-					<p>19:00 - 21:00</p>
 				</div>
 			</div>
-		</div>		
+		</div>
 	</div>
-	<div class="col-md-6 col-md-offset-3" style="text-align: center; margin-bottom: 28px;">
-		<a href="{{url('/reservasi')}}" class="btn btn-primary">RESERVASI</a>
-	</div>		
+</div>
 @endsection
 
 @section('moreScript')
